c9c2c381c9db570d2f3dfe4213ecd24416c2cef0
erehmaryann <edwardmaryann09@gmail.com>
2022-06-16 10:46:46 -0700

    fixed manifest error

--- public/service-worker.js
+++ public/service-worker.js
@@ -45,7 +45,6 @@ self.addEventListener("activate", event => {
                 if (!cacheWhitelist.includes(cacheName)) {
                     return caches.delete(cacheName);
                 }
-                // return null;
             })
         ))
 
